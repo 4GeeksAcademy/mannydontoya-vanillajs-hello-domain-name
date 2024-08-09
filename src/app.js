8655f86f5da35a25eda0d4a19de356fa55df919e
Daniel Montoya <153567770+mannydontoya@users.noreply.github.com>
2024-08-09 23:28:21 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -2,10 +2,23 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+  let pronouns = ["the", "our"];
+  let adjs = ["great", "big"];
+  let nouns = ["jogger", "racoon"];
+  let domains = [".net", ".com", ".org", ".gov"];
+  for (let pronoun of pronouns) {
+    for (let adj of adjs) {
+      for (let noun of nouns) {
+        for (let domain of domains) {
+          document.querySelector("#domainGen").innerHTML +=
+            "<li><h1>" + pronoun + adj + noun + domain + "</h1></li>";
+        }
+      }
+    }
+  }
+
+  console.log();
 };
